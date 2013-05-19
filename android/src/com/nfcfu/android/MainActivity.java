facb50a48cac8d04f2edfa0d0761f24924ea3ac1
Tony Grosinger <tony@grosinger.net>
2013-05-18 18:12:02 -0700

    Finished with writing to NFC Tag

--- android/src/com/nfcfu/android/MainActivity.java
+++ android/src/com/nfcfu/android/MainActivity.java
@@ -2,30 +2,25 @@ package com.nfcfu.android;
 
 import android.app.Activity;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.nfc.*;
-import android.nfc.tech.Ndef;
+import android.nfc.FormatException;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends Activity {
     NfcAdapter adapter;
     PendingIntent pendingIntent;
     IntentFilter writeTagFilters[];
-    boolean writeMode;
-    Tag mytag;
-    Context ctx;
+
+    Tag tagToWrite;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,19 +29,20 @@ public class MainActivity extends Activity {
         Log.v("NFCFU", "Creating activity");
 
         setContentView(R.layout.activity_main);
-        ctx = this;
         Button btnWrite = (Button) findViewById(R.id.writeNfcButton);
 
         btnWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    if (mytag == null) {
+                    if (tagToWrite == null) {
                         Log.e("NFCFU", "Error Detected");
                     } else {
-                        write(getIpAddr(), mytag);
+                        NfcHelper.writeIpAddress(tagToWrite, (WifiManager) getSystemService(WIFI_SERVICE));
                         Log.v("NFCFU", "NFC Tag Written");
                     }
+                } catch (IllegalStateException e) {
+
                 } catch (IOException e) {
                     Log.e("NFCFU", "Error Writing");
                     e.printStackTrace();
@@ -57,86 +53,35 @@ public class MainActivity extends Activity {
             }
         });
 
+        // Create adaptor to listen for NFC tag
         adapter = NfcAdapter.getDefaultAdapter(this);
+
+        // Create the intent that we are listening for
         pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // Filter intents
         IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
         tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
         writeTagFilters = new IntentFilter[]{tagDetected};
-        Log.v("NFCFU", "Finished Setup");
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
-            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            tagToWrite = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             Log.v("NFCFU", "Detected new Intent");
         }
-    }
-
-    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
-
-        //create the message in according with the standard
-        String lang = "en";
-        byte[] textBytes = text.getBytes();
-        byte[] langBytes = lang.getBytes("US-ASCII");
-        int langLength = langBytes.length;
-        int textLength = textBytes.length;
-
-        byte[] payload = new byte[1 + langLength + textLength];
-        payload[0] = (byte) langLength;
-
-        // copy langbytes and textbytes into payload
-        System.arraycopy(langBytes, 0, payload, 1, langLength);
-        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
-
-        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
-        return recordNFC;
-    }
-
-    private void write(String text, Tag tag) throws IOException, FormatException {
-
-        NdefRecord[] records = {createRecord(text)};
-        NdefMessage message = new NdefMessage(records);
-        Ndef ndef = Ndef.get(tag);
-        ndef.connect();
-        ndef.writeNdefMessage(message);
-        ndef.close();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        WriteModeOff();
+        adapter.disableForegroundDispatch(this);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        WriteModeOn();
-    }
-
-    private void WriteModeOn() {
-        writeMode = true;
         adapter.enableForegroundDispatch(this, pendingIntent, writeTagFilters, null);
-    }
-
-    private void WriteModeOff() {
-        writeMode = false;
-        adapter.disableForegroundDispatch(this);
-    }
-
-    public String getIpAddr() {
-        Log.v("NFCFU", "Getting IP Address");
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        int ip = wifiInfo.getIpAddress();
-
-        String ipString = String.format("%d.%d.%d.%d",
-                (ip & 0xff),
-                (ip >> 8 & 0xff),
-                (ip >> 16 & 0xff),
-                (ip >> 24 & 0xff));
-
-        return ipString;
     }
 }
---
+++ android/src/com/nfcfu/android/NfcHelper.java
@@ -0,0 +1,81 @@
+package com.nfcfu.android;
+
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author Tony Grosinger
+ *         <p/>
+ *         Helper functions for writing data to an NFC tag from an Android app
+ */
+public class NfcHelper {
+
+    private NfcHelper() {
+    }
+
+    /**
+     * Write the current IP address of the Android device to an NFC Tag. Must be connected to wifi network.
+     *
+     * @param tag  NFC Tag to write message to
+     * @param wifi WifiManager which can be used to retrieve IP address
+     * @throws IOException
+     * @throws FormatException
+     * @throws IllegalStateException when Android device is not connected to Wifi network
+     */
+    public static void writeIpAddress(Tag tag, WifiManager wifi) throws IOException, FormatException, IllegalStateException {
+        String ipAddress = getIpAddr(wifi);
+        if (ipAddress.equals("0.0.0.0")) {
+            throw new IllegalStateException("Not connected to Wifi");
+        }
+
+        NdefRecord[] records = {createRecord(ipAddress)};
+        NdefMessage message = new NdefMessage(records);
+        Ndef ndef = Ndef.get(tag);
+        ndef.connect();
+        ndef.writeNdefMessage(message);
+        ndef.close();
+    }
+
+    private static NdefRecord createRecord(String message) throws UnsupportedEncodingException {
+        //create the message in according with the standard
+        String lang = "en";
+        byte[] textBytes = message.getBytes();
+        byte[] langBytes = lang.getBytes("US-ASCII");
+        int langLength = langBytes.length;
+        int textLength = textBytes.length;
+
+        byte[] payload = new byte[1 + langLength + textLength];
+        payload[0] = (byte) langLength;
+
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1, langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
+        return recordNFC;
+    }
+
+    private static String getIpAddr(WifiManager wifiManager) {
+        Log.v(NfcHelper.class.getSimpleName(), "Getting IP Address");
+
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ip = wifiInfo.getIpAddress();
+
+        String ipString = String.format("%d.%d.%d.%d",
+                (ip & 0xff),
+                (ip >> 8 & 0xff),
+                (ip >> 16 & 0xff),
+                (ip >> 24 & 0xff));
+
+        return ipString;
+    }
+}
