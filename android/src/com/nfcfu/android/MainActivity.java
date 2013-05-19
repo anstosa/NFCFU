03246b643e157bf4298f2df21569675d56b5a6a1
Tony Grosinger <tony@grosinger.net>
2013-05-18 17:32:06 -0700

    Writing IP address to tag

--- android/src/com/nfcfu/android/MainActivity.java
+++ android/src/com/nfcfu/android/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.nfc.*;
 import android.nfc.tech.Ndef;
 import android.os.Bundle;
@@ -32,18 +34,17 @@ public class MainActivity extends Activity {
         Log.v("NFCFU", "Creating activity");
 
         setContentView(R.layout.activity_main);
-        ctx=this;
+        ctx = this;
         Button btnWrite = (Button) findViewById(R.id.writeNfcButton);
 
-        btnWrite.setOnClickListener(new View.OnClickListener()
-        {
+        btnWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    if(mytag==null){
+                    if (mytag == null) {
                         Log.e("NFCFU", "Error Detected");
-                    }else{
-                        write("This is the message 2", mytag);
+                    } else {
+                        write(getIpAddr(), mytag);
                         Log.v("NFCFU", "NFC Tag Written");
                     }
                 } catch (IOException e) {
@@ -60,13 +61,13 @@ public class MainActivity extends Activity {
         pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
         tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
-        writeTagFilters = new IntentFilter[] { tagDetected };
+        writeTagFilters = new IntentFilter[]{tagDetected};
         Log.v("NFCFU", "Finished Setup");
     }
 
     @Override
-    protected void onNewIntent(Intent intent){
-        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+    protected void onNewIntent(Intent intent) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
             mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             Log.v("NFCFU", "Detected new Intent");
         }
@@ -94,7 +95,7 @@ public class MainActivity extends Activity {
 
     private void write(String text, Tag tag) throws IOException, FormatException {
 
-        NdefRecord[] records = { createRecord(text) };
+        NdefRecord[] records = {createRecord(text)};
         NdefMessage message = new NdefMessage(records);
         Ndef ndef = Ndef.get(tag);
         ndef.connect();
@@ -103,82 +104,39 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         WriteModeOff();
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         WriteModeOn();
     }
 
-    private void WriteModeOn(){
+    private void WriteModeOn() {
         writeMode = true;
         adapter.enableForegroundDispatch(this, pendingIntent, writeTagFilters, null);
     }
 
-    private void WriteModeOff(){
+    private void WriteModeOff() {
         writeMode = false;
         adapter.disableForegroundDispatch(this);
     }
 
-/*
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public String getIpAddr() {
+        Log.v("NFCFU", "Getting IP Address");
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ip = wifiInfo.getIpAddress();
 
-        Log.v("NFCFU", "Creating Activity");
+        String ipString = String.format("%d.%d.%d.%d",
+                (ip & 0xff),
+                (ip >> 8 & 0xff),
+                (ip >> 16 & 0xff),
+                (ip >> 24 & 0xff));
+
+        return ipString;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    private void write(Tag tag) throws IOException, FormatException {
-        NdefRecord[] records = { createRecord() };
-        NdefMessage message = new NdefMessage(records);
-
-        // Get an instance of Ndef for the tag.
-        Ndef ndef = Ndef.get(tag);
-
-        // Enable I/O
-        ndef.connect();
-
-        // Write the message
-        ndef.writeNdefMessage(message);
-
-        // Close the connection
-        ndef.close();
-    }
-
-
-    private NdefRecord createRecord() throws UnsupportedEncodingException {
-        String text       = "Hello, World!";
-        String lang       = "en";
-        byte[] textBytes  = text.getBytes();
-        byte[] langBytes  = lang.getBytes("US-ASCII");
-        int    langLength = langBytes.length;
-        int    textLength = textBytes.length;
-        byte[] payload    = new byte[1 + langLength + textLength];
-
-        // set status byte (see NDEF spec for actual bits)
-        payload[0] = (byte) langLength;
-
-        // copy langbytes and textbytes into payload
-        System.arraycopy(langBytes, 0, payload, 1,              langLength);
-        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
-
-        NdefRecord record = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,
-                NdefRecord.RTD_TEXT,
-                new byte[0],
-                payload);
-
-        return record;
-    }
-    */
 }
